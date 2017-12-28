f6dffa407645f5419219c5861dc1820a37ce042f
venaseph <cperagine@gmail.com>
2017-12-28 08:58:50 -0500

    Key added to okhttp request

--- app/src/main/java/com/venaseph/kt_hearthmarket/MainActivity.kt
+++ app/src/main/java/com/venaseph/kt_hearthmarket/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,31 @@ class MainActivity : AppCompatActivity() {
         // recView_main.setBackgroundColor(Color.BLUE)
         recView_main.layoutManager = LinearLayoutManager(this)
         recView_main.adapter = MainAdapter()
+
+        fetchJson()
+
+    }
+
+    private fun fetchJson() {
+        //println("Attempting to Fetch JSON")
+
+        val keyVal = "mSCkyyFuiMmshxkH3QTA6RsLglGGp1GG15ajsnJdGUUoajgNgt"
+        val url = "https://omgvamp-hearthstone-v1.p.mashape.com/cards"
+        // make request object
+        val request = Request.Builder().header("X-Mashape-Key", keyVal).url(url).build()
+        //make request client
+        val client = OkHttpClient()
+        //create request (can't execute http requests on main thread in android so enqueue
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+        })
 
     }
 }
